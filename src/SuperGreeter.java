6e8a9d23f85825d226cf68359a353a1f5ae90a09
nicolaiandersson <nico602d.stud.kea.dk>
2022-09-09 13:34:50 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,12 @@
+public class SuperGreeter {
+    public void start() {
+        System.out.println("Det virker!");
+    }
+
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+
+    }
+
+}
