e962fb26bce8e030d10cae5352ce53474de63399
nicolaiandersson <nico602d.stud.kea.dk>
2022-09-09 13:43:52 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
 public class SuperGreeter {
+
     public void start() {
         System.out.println("Det virker!");
     }
@@ -8,12 +8,19 @@ public class SuperGreeter {
         Scanner scan = new Scanner(System.in);
         System.out.println("Indtast dit navn:");
         String navn = scan.next();
-        System.out.println("Hej" + navn);
+        System.out.println("Hej " + navn);
+    }
+    public void askAboutAge() {
+        System.out.println("Indtast din alder:");
+        Scanner scan = new Scanner(System.in);
+        int alder = scan.nextInt();
+        System.out.println(alder);
     }
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
         sg.greet();
+        sg.askAboutAge();
     }
 
 }
