069d6dfbbd45e7228ef72b941c906b13666c7ba5
nicolaiandersson <nico602d.stud.kea.dk>
2022-09-09 13:39:22 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start() {
         System.out.println("Det virker!");
     }
-
+    public void greet() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Indtast dit navn:");
+        String navn = scan.next();
+        System.out.println("Hej" + navn);
+    }
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
-
+        sg.greet();
     }
 
 }
